cdf9319e895ce938b43c2d86f98f5cbdd1aaa89b
SandeepVarmaManthena <manthenasandeepvarma766@gmail.com>
2023-10-07 09:26:10 +0530

    signup page edited

--- signup.php
+++ signup.php
@@ -1,8 +1,20 @@
-company name
-email
-password
+
 <div class="signup_container">
     <form action="">
-        <input type="text" placeholder="" required>
+        <div class="cmpny_nme">
+            <label for="">Company Name:</label>
+            <input type="text" placeholder="" required>
+        </div>
+        <div class="eml">
+            <label for="email">Email:</label>
+            <input type="email" name="" id="" required>
+        </div>
+        <div class="psswrd">
+            <label for="email">Email:</label>
+            <input type="password" name="" id="" required>
+        </div>
+        <div class="signup-btn">
+            <input type="submit" value="">
+        </div>
     </form>
 </div>
